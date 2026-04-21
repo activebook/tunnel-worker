18f7e6ea8e11099aa6146d2728d8857e8347137e
Charles Liu <xinasuka@126.com>
2026-04-22 07:08:33 +0800

    refactor: update entry point to src/worker.ts and rename project to tunnel-worker

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 
 // For testing purpose only, will be removed in the future
 export default {
-  async fetch(request: Request): Promise<Response> {
-    console.log('[TEST] reached fetch handler');
-    return new Response('ok', { status: 200 });
-  }
+  // async fetch(request: Request): Promise<Response> {
+  //   console.log('[TEST] reached fetch handler');
+  //   return new Response('ok', { status: 200 });
+  // }
 };
